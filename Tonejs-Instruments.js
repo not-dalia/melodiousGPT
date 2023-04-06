15992046183cf7d10e453201c21ced7ad8e5b6ed
not-dalia <32544547+not-dalia@users.noreply.github.com>
2023-04-06 11:08:48 +0100

    Update Tonejs-Instruments.js

--- Tonejs-Instruments.js
+++ Tonejs-Instruments.js
@@ -22,7 +22,7 @@ SAMPLES RELEASED UNDER CC-BY 3.0
 var SampleLibrary = {
     minify: false,
     ext: '.[mp3|ogg]', // use setExt to change the extensions on all files // do not change this variable //
-    baseUrl: '/samples/',
+    baseUrl: '/melodiousGPT/samples/',
     list: ['bass-electric','bassoon','cello','clarinet','contrabass','flute','french-horn','guitar-acoustic','guitar-electric','guitar-nylon', 'harmonium','harp','organ','piano','saxophone','trombone','trumpet','tuba','violin','xylophone'],
     onload: null,
 
